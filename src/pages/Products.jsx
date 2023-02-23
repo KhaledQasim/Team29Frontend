1ea1d7bb077983cd2a00b22003f522c50489c14e
Salma Tariq <116220007+reveluv5@users.noreply.github.com>
2023-02-23 11:31:50 +0000

    added another row of sample products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -76,23 +76,23 @@ function App() {
             <MDBCardImage
               src={image}
               position="top"
-              alt="Laptop"
+              alt="Jeans"
             />
             <MDBCardBody>
               <div className="d-flex justify-content-between">
                 <p className="small">
                   <a href="#!" className="text-muted">
-                    Laptops
+                    Jeans
                   </a>
                 </p>
                 <p className="small text-danger">
-                  <s>$1199</s>
+                  <s>£70.99</s>
                 </p>
               </div>
 
               <div className="d-flex justify-content-between mb-3">
-                <h5 className="mb-0">HP Envy</h5>
-                <h5 className="text-dark mb-0">$1099</h5>
+                <h5 className="mb-0">WeWear Jeans</h5>
+                <h5 className="text-dark mb-0">£49.99</h5>
               </div>
 
               <div class="d-flex justify-content-between mb-2">
@@ -124,23 +124,170 @@ function App() {
             <MDBCardImage
               src={image}
               position="top"
-              alt="Gaming Laptop"
+              alt="Jackets"
             />
             <MDBCardBody>
               <div className="d-flex justify-content-between">
                 <p className="small">
                   <a href="#!" className="text-muted">
-                    Laptops
+                    Jackets
                   </a>
                 </p>
                 <p className="small text-danger">
-                  <s>$1399</s>
+                  <s>£89.99</s>
                 </p>
               </div>
 
               <div className="d-flex justify-content-between mb-3">
-                <h5 className="mb-0">Toshiba B77</h5>
-                <h5 className="text-dark mb-0">$1299</h5>
+                <h5 className="mb-0">WeWear Jacket</h5>
+                <h5 className="text-dark mb-0">£50.00</h5>
+              </div>
+
+              <div class="d-flex justify-content-between mb-2">
+                <p class="text-muted mb-0">
+                  Available: <span class="fw-bold">5</span>
+                </p>
+                <div class="ms-auto text-warning">
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star-half-alt" />
+                </div>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+      <br></br>
+      <MDBRow>
+        <MDBCol md="12" lg="4" className="mb-4 mb-lg-0">
+          <MDBCard>
+            {/* <div className="d-flex justify-content-between p-3">
+              <p className="lead mb-0">Today's Combo Offer</p>
+              <div
+                className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
+                style={{ width: "35px", height: "35px" }}
+              >
+                <p className="text-white mb-0 small">x4</p>
+              </div>
+            </div> */}
+            <MDBCardImage
+              src={image}
+              position="top"
+              alt="T-Shirt"
+            />
+            <MDBCardBody>
+              <div className="d-flex justify-content-between">
+                <p className="small">
+                  <a href="#!" className="text-muted">
+                    T-Shirts
+                  </a>
+                </p>
+                <p className="small text-danger">
+                  <s>£100.99</s>
+                </p>
+              </div>
+
+              <div className="d-flex justify-content-between mb-3">
+                <h5 className="mb-0">WeWear T-Shirt</h5>
+                <h5 className="text-dark mb-0">£80.00</h5>
+              </div>
+
+              <div class="d-flex justify-content-between mb-2">
+                <p class="text-muted mb-0">
+                  Available: <span class="fw-bold">6</span>
+                </p>
+                <div class="ms-auto text-warning">
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                </div>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md="12" lg="4" className="mb-4 mb-lg-0">
+          <MDBCard>
+            {/* <div className="d-flex justify-content-between p-3">
+              <p className="lead mb-0">Today's Combo Offer</p>
+              <div
+                className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
+                style={{ width: "35px", height: "35px" }}
+              >
+                <p className="text-white mb-0 small">x2</p>
+              </div>
+            </div> */}
+            <MDBCardImage
+              src={image}
+              position="top"
+              alt="Jeans"
+            />
+            <MDBCardBody>
+              <div className="d-flex justify-content-between">
+                <p className="small">
+                  <a href="#!" className="text-muted">
+                    Jeans
+                  </a>
+                </p>
+                <p className="small text-danger">
+                  <s>£70.99</s>
+                </p>
+              </div>
+
+              <div className="d-flex justify-content-between mb-3">
+                <h5 className="mb-0">WeWear Jeans</h5>
+                <h5 className="text-dark mb-0">£49.99</h5>
+              </div>
+
+              <div class="d-flex justify-content-between mb-2">
+                <p class="text-muted mb-0">
+                  Available: <span class="fw-bold">7</span>
+                </p>
+                <div class="ms-auto text-warning">
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon far icon="star" />
+                </div>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md="12" lg="4" className="mb-4 mb-lg-0">
+          <MDBCard>
+            {/* <div className="d-flex justify-content-between p-3">
+              <p className="lead mb-0">Today's Combo Offer</p>
+              <div
+                className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
+                style={{ width: "35px", height: "35px" }}
+              >
+                <p className="text-white mb-0 small">x3</p>
+              </div>
+            </div> */}
+            <MDBCardImage
+              src={image}
+              position="top"
+              alt="Jackets"
+            />
+            <MDBCardBody>
+              <div className="d-flex justify-content-between">
+                <p className="small">
+                  <a href="#!" className="text-muted">
+                    Jackets
+                  </a>
+                </p>
+                <p className="small text-danger">
+                  <s>£89.99</s>
+                </p>
+              </div>
+
+              <div className="d-flex justify-content-between mb-3">
+                <h5 className="mb-0">WeWear Jacket</h5>
+                <h5 className="text-dark mb-0">£50.00</h5>
               </div>
 
               <div class="d-flex justify-content-between mb-2">
