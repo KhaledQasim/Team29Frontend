6678ff0cc639861d2b5613a46f7900c0642758dd
KhaledQasim <khaled.qasim183@gmail.com>
2023-02-27 22:10:44 +0000

    Navbar home now links to home

--- src/navbar/index.js
+++ src/navbar/index.js
@@ -132,9 +132,9 @@ function Navbarr() {
       <div className="collapse navbar-collapse order-lg-1" id="navMenu">
         <ul className="navbar-nav mx-auto text-center">
           <li className="nav-item px-2 py-2">
-            <a className="nav-link text-uppercase text-dark" href="/">
+            <Link className="nav-link text-uppercase text-dark" to="/">
               home
-            </a>
+            </Link>
           </li>
           <li className="nav-item px-2 py-2">
             <Link className="nav-link text-uppercase text-dark" to="/products">
