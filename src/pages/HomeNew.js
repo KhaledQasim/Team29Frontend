ca307889a360fe335bace2c6d7e32a17d6efe94b
basmomo <116265987+basmomo@users.noreply.github.com>
2023-03-23 23:34:02 +0000

    Linking the new page, removing the extras from the homepage

--- src/pages/HomeNew.js
+++ src/pages/HomeNew.js
@@ -560,32 +560,7 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className="contact-us-container">
-      <div className="container">
-        <div className="title text-center">
-               <h2 className="position-relative d-inline-block">Contact Us</h2>
-        </div>
-        <div className="row">
-          <div className="col-md-8 offset-md-2">
-            <form>
-              <div className="mb-3">
-                <label htmlFor="name" className="form-label">Name</label>
-                <input type="text" className="form-control" id="name" />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email</label>
-                <input type="email" className="form-control" id="email" />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="message" className="form-label">Message</label>
-                <textarea className="form-control" id="message" rows="5"></textarea>
-              </div>
-              <button type="submit" className="btn">Submit</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+      
 
     </div>
   </section>
