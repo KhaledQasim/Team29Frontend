e65c7bf0b1658389d299a455582d8ec5973d6252
Salma <210114232@aston.ac.uk>
2023-03-25 22:17:11 +0000

    modified login page

--- src/login/index.js
+++ src/login/index.js
@@ -1,3 +1,17 @@
+import React from "react";
+import {
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBInputGroup,
+  MDBCheckbox
+}
+from 'mdb-react-ui-kit';
+
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
@@ -11,6 +25,7 @@ import { faEye } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import Cookies from "js-cookie";
 import configData from "../config.json";
+import image from "../products/images/we_WEAR-8.png";
 const eye = <FontAwesomeIcon icon={faEye} />;
 
 function Login() {
@@ -59,7 +74,7 @@ function Login() {
   return (
     <>
     
-      <Form className="content-container">
+      {/* <Form className="content-container">
       <Row className="mb-3">
       <Form.Group className="mb-3" controlId="email" as={Col} md="6">
         <Form.Label>Email address</Form.Label>
@@ -123,7 +138,98 @@ function Login() {
       >
         Cancel
       </Button>
-    </Form>
+    </Form> */}
+
+<MDBContainer className='my-5'>
+      <MDBCard>
+
+        <MDBRow className='g-0 d-flex align-items-center'>
+
+          <MDBCol md='4'>
+            <MDBCardImage src={image} alt='logo' className='rounded-t-5 rounded-tr-lg-0' fluid />
+          </MDBCol>
+
+          <MDBCol md='8'>
+
+            <MDBCardBody>
+
+            
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <MDBInputGroup>
+                  <Form.Control
+                    type={passwordShown ? "text" : "password"}
+                    // type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </MDBInputGroup>
+                
+              </Form.Group>
+
+              <div className="d-flex justify-content-between mx-4 mb-4">
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+                <a href="!#">Forgot password?</a>
+              </div>
+                
+              {errorMsg ? (
+                <Container>
+                    <Row className="justify-content-center mb-4">
+                      <Col md="8" lg="6">
+                        <div className="" style={{ color: "red", fontWeight: "bold" }}>
+                          {errorMsg}
+                        </div>
+                      </Col>
+                    </Row>
+                </Container>
+                ) : (
+                  <></>
+                )}
+            
+            <Button
+            id="submit"
+            variant="primary"
+            type="button"
+            disabled={disabled}
+            // onClick={() => sendLoginRequest()}
+            onClick={handleClick}
+          >
+            Login
+          </Button>
+          <Button
+            className="mx-2"
+            id="exit"
+            variant="secondary"
+            type="button"
+            // onClick={() => sendLoginRequest()}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Cancel
+            </Button>
+
+            </MDBCardBody>
+
+          </MDBCol>
+
+        </MDBRow>
+
+      </MDBCard>
+    </MDBContainer>
+
+
     </>
    
   );
