285647c0d4e416e901e08add92dfeaca4c323ec3
KhaledQasim <khaled.qasim183@gmail.com>
2023-03-27 14:49:38 +0100

    Fixed Collection CSS

--- src/products/Products.jsx
+++ src/products/Products.jsx
@@ -4,31 +4,56 @@ import { Card, Button, CardGroup, Row } from "react-bootstrap";
 import "../App.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import styled from "styled-components";
 // import TshirtPlaceholder from "../images/PlaceHolder/TshirtPlaceholder.png";
 // import TshirtPlaceholder from "/images/PlaceHolder/TshirtPlaceholder.png";
 // import JumperPlaceholder from "../../public/images/PlaceHolder/TshirtPlaceholder.png";
 // import JeansPlaceholder from "../../public/images/PlaceHolder/TshirtPlaceholder.png";
 // import ShortsPlaceholder from "../../public/images/PlaceHolder/TshirtPlaceholder.png";
 // import JacketPlaceholder from "../../public/images/PlaceHolder/TshirtPlaceholder.png";
-export default function Products() {
- 
-  const navigate = useNavigate();
- 
+const Container = styled.div`
+  margin-left: 1rem;
+  margin-top : 1rem;
   
- 
+`;
+
+const StyledCard = styled(Card)`
+  display: flex;
+  
+  flex: 1;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+  @media (max-width: 1200px) {
+    flex: 50%;
+    margin-bottom: 1rem;
+  }
+  @media (max-width: 800px) {
+    flex: 100%;
+    margin-bottom: 1rem;
+  }
+`;
+
+const CardImage = styled(Card.Img)`
+  flex-grow: inherit;
+  
+`;
+
+// style={{ width: "18rem", marginRight: "1rem", flex:1 }}
+export default function Products() {
+  const navigate = useNavigate();
 
   return (
-    <div className="content-container" style={{ marginLeft: "1rem" }}>
-      <Row xs={2} md={5} className="g-0">
-        <Card style={{ width: "18rem", marginRight: "1rem" }}>
-          <Card.Img
+    <Container className="content-container">
+      <Row xs={2} md={2} xl={2} className="g-0">
+        <StyledCard className="card">
+          <CardImage
             variant="top"
-            style={{ flexGrow: "inherit" }}
+            style={{}}
             src="/images/PlaceHolder/ShortsPlaceholder.png"
           />
           <Card.Body>
             <Card.Title style={{ fontSize: "3rem" }}>Shorts</Card.Title>
-            {/* <Card.Text>
+            {/* <Card.Text>x
                 View all of our items in our WeWear T-Shirts product category.
               </Card.Text> */}
             {/* to="/products/t-shirts" */}
@@ -40,12 +65,12 @@ export default function Products() {
               View Shorts
             </Button>
           </Card.Body>
-        </Card>
+        </StyledCard>
 
-        <Card style={{ width: "18rem", marginRight: "1rem" }}>
-          <Card.Img
+        <StyledCard>
+          <CardImage
             variant="top"
-            style={{ flexGrow: "inherit" }}
+            style={{}}
             src="/images/PlaceHolder/TshirtPlaceholder.png"
           />
           <Card.Body>
@@ -62,12 +87,12 @@ export default function Products() {
               View T-Shirts
             </Button>
           </Card.Body>
-        </Card>
+        </StyledCard>
 
-        <Card style={{ width: "18rem", marginRight: "1rem" }}>
-          <Card.Img
+        <StyledCard>
+          <CardImage
             variant="top"
-            style={{ flexGrow: "inherit" }}
+            style={{}}
             src="/images/PlaceHolder/JacketPlaceholder.png"
           />
           <Card.Body>
@@ -84,12 +109,12 @@ export default function Products() {
               View Jackets
             </Button>
           </Card.Body>
-        </Card>
+        </StyledCard>
 
-        <Card style={{ width: "18rem", marginRight: "1rem" }}>
-          <Card.Img
+        <StyledCard>
+          <CardImage
             variant="top"
-            style={{ flexGrow: "inherit" }}
+            style={{}}
             src="/images/PlaceHolder/JeansPlaceholder.png"
           />
           <Card.Body>
@@ -106,12 +131,12 @@ export default function Products() {
               View Jeans
             </Button>
           </Card.Body>
-        </Card>
+        </StyledCard>
 
-        <Card style={{ width: "18rem", marginRight: "1rem" }}>
-          <Card.Img
+        <StyledCard>
+          <CardImage
             variant="top"
-            style={{ flexGrow: "inherit" }}
+            style={{}}
             src="/images/PlaceHolder/SweaterPlaceholder.png"
           />
           <Card.Body>
@@ -128,10 +153,8 @@ export default function Products() {
               View Jumpers
             </Button>
           </Card.Body>
-        </Card>
-
-       
+        </StyledCard>
       </Row>
-    </div>
+    </Container>
   );
 }
