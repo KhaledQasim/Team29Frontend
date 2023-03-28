b718200eef37bfa9ac3883b21700e3b8eca3f30f
Dabakeh Tangara <dktk@hotmail.co.uk>
2023-03-28 14:07:05 +0100

    adding search to products

--- src/products/CategoryProducts.jsx
+++ src/products/CategoryProducts.jsx
@@ -1,45 +1,52 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Card, Row } from "react-bootstrap";
+import { Button, Card, Form, Row } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
 
 const CategoryProducts = () => {
   const { category } = useParams();
   const navigate = useNavigate();
-  const [products, SetProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
   const loadProductsByCategory = async () => {
     await axios.get(`/productsCategory/${category}`)
-      .then((data) => SetProducts(data.data));
-     
+      .then((data) => setProducts(data.data));
   };
+
   useEffect(() => {
     loadProductsByCategory();
-    
   }, []);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredProducts = products.filter(product =>
+    product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div className="content-container" style={{ marginLeft: "1rem" }}>
-      <Row xs={2} md={5} className="g-0" >
-        {products.map((product) => (
-          <Card style={{ width: "18rem", marginRight: "1rem" , flexGrow:"1",marginBottom:"1rem"}}>
+      <Form>
+        <Form.Control
+          type="text"
+          placeholder="Search products by name"
+          value={searchQuery}
+          onChange={handleSearch}
+        />
+      </Form>
+      <Row xs={2} md={5} className="g-0">
+        {filteredProducts.map((product) => (
+          <Card style={{ width: "18rem", marginRight: "1rem", flexGrow: "1", marginBottom: "1rem" }}>
             <Card.Img
               variant="top"
               style={{ flexGrow: "inherit" }}
               src={product.image}
             />
             <Card.Body>
-              <Card.Title style={{fontSize:"3rem"}} >{product.name}</Card.Title>
-              {/* <Card.Text>
-                View all of our items in our WeWear T-Shirts product category.
-              </Card.Text> */}
-              {/* to="/products/t-shirts" */}
-              <Button   
-                onClick={() => {
-                  navigate(`/products/${product.id}`);
-                }}
-              >
-                View Item
-              </Button>
+              <Card.Title style={{ fontSize: "3rem" }}>{product.name}</Card.Title>
+              <Button onClick={() => navigate(`/products/${product.id}`)}>View Item</Button>
             </Card.Body>
           </Card>
         ))}
